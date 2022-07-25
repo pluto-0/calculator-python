cc7b2a424f0a34f63144e305c4562df436d841e1
Adam Aguiar <adamaguiar10@gmail.com>
2022-07-25 16:56:59 -0400

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -1,9 +1,11 @@
 def gather_inputs():
     operation = input("Please type in operation you would like to complete: +, -, *, or /")
+# casting to int doesnt work if string is input, also why no support for float?
     number_1 = int(input('Please enter the first number: '))
     number_2 = int(input('Please enter the second number: '))
     return operation, number_1, number_2
 
+# elifs could be ifs because of return statements, else unecessary, simplifying would be more pythonic
 def run_operation(number_1,  number_2, operation):
     if operation == '+':
         print('{} + {} = '.format(number_1, number_2), end = '')
@@ -14,6 +16,7 @@ def run_operation(number_1,  number_2, operation):
     elif operation == '*':
         print('{} * {} = '.format(number_1, number_2), end = '')
         return number_1 * number_2
+# Does not consider division by 0
     elif operation == '/':
         print('{} / {} = '.format(number_1, number_2), end = '')
         return number_1 / number_2
